166f4169a19a019c9112b8a6c839247e6c00290a
Victor Cazacov <cazacov.git@gmail.com>
2014-09-21 17:09:19 +0200

    Fine tuning PIDs

--- BalancingRobot/BalancingRobot/Main.cpp
+++ BalancingRobot/BalancingRobot/Main.cpp
@@ -31,8 +31,8 @@ void setup()
 	motors.init();
 	gyroscope.init();
 	keyboard.init();
-	angleRegulator.init(-0.1, 0, 0);
-	speedRegulator.init(30, 0, 0);
+	angleRegulator.init(-0.02, 0, 0);
+	speedRegulator.init(35, 0.2, 0);
 	wprintf(L"Press Esc to exit\n");
 }
 
